ca9c90ee7821d21ab822539a84313ca385d96b10
tfobadar <89089416+tfobadar@users.noreply.github.com>
2024-08-10 19:00:43 +0300

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,29 @@
 import { defineConfig } from "astro/config";
 import storyblok from "@storyblok/astro";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
 import { loadEnv } from "vite";
 const { PUBLIC_ENV, STORYBLOK_TOKEN_ACCESS} = loadEnv(import.meta.env.MODE, process.cwd(), "");
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
-  output: PUBLIC_ENV === 'preview' ? 'server' : 'static',
-  adapter: PUBLIC_ENV === 'preview' ? netlify() : undefined,
+
   integrations: [
     storyblok({
       accessToken: STORYBLOK_TOKEN_ACCESS,
-      bridge: PUBLIC_ENV !== 'production',
       components: {
         page: "storyblok/Page",
         feature: "storyblok/Feature",
         grid: "storyblok/Grid",
         teaser: "storyblok/Teaser",
+        hero: 'storyblok/Hero',
+        aboutSection: 'storyblok/AboutSection',
+        spacer: 'storyblok/Spacer',
+        CTABanner: 'storyblok/CTABanner',
+        titleSection: 'storyblok/TitleSection',
+        cards: 'storyblok/Cards',
+        card: 'storyblok/Card',
+        ourTeam: 'storyblok/OurTeam',
+        image: 'storyblok/Image',
       },
     }),
     tailwind(),
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary' :'#f1c152',
+				'secondary': '#737373',
+				'body': '#737373',
+				'muted': '#6c757d',
+				'light': '#f8f9fa',
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+	],
 }
